c1a8dff2ee53cf9dd1f4fb682864c623a0050aae
leebird <leemagpie@gmail.com>
2014-07-20 18:27:48 -0400

    ubuntu script

---
+++ ubuntu.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# update repo
+sudo apt-get update
+
+# common softwares
+sudo apt-get -y -q install gcc g++ emacs23 git wget sshfs parallel python-setuptools
+
+# pip
+sudo easy_install pip
+
+## nltk
+sudo pip install -U nltk
+# download sentence splitter punkt
+python -m nltk.downloader punkt
+
+## charniak parser
+# dependency
+sudo apt-get -y -q install python-twisted-web python2.7-dev
+#parser
+sudo pip install bllipparser
+# make folders
+mkdir sshfs
+mkdir download
+mkdir local
+# download charniak parser biomedical model
+wget http://bllip.cs.brown.edu/download/bioparsingmodel-rel1.tar.gz -O download/biomedical.tar.gz
+tar -zxvf download/biomedical.tar.gz -C local/
+
+# connect sshfs
+sshfs leebird@68.80.91.56:/home/leebird/amazon/sshfs ~/sshfs
